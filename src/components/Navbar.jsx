be154c155037dca6f48bb43fc1318d4bc57e0a56
Anuraaaa <anwarfauzan75@gmail.com>
2023-05-23 02:12:26 +0700

    decoration and href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar () {
     return (
         <nav className="navbar">
-            <a className="navbar-title">Tuna Grahita Educate</a>
+            <a href="#" className="navbar-title">Tuna Grahita Educate</a>
             <ul className="navbar-content">
                 <li><a href="#">About</a></li>
                 <li><a href="#">Guide</a></li>
