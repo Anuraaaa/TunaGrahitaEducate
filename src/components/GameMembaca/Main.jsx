3e7c56a6b29d84cb296d73dbc77d38f62ddfde17
Anuraaaa <anwarfauzan75@gmail.com>
2023-05-25 02:31:57 +0700

    Add switch item game membaca

--- src/components/GameMembaca/Main.jsx
+++ src/components/GameMembaca/Main.jsx
@@ -8,98 +8,52 @@ import mango from "../../asset/img/mango.png";
 import mangoputus from "../../asset/img/mangoputus.png";
 import ceri from "../../asset/img/ceri.png";
 import ceriputus from "../../asset/img/ceriputus.png";
+import { useEffect, useState } from "react";
 
 export default function Main() {
 
-    let randomPick = Math.floor((Math.random() * 5));
+    const fruitName = ["A-p-e-l", "P-i-s-a-n-g", "A-n-g-g-u-r", "M-a-n-g-g-a", "C-e-r-i"];
+    const fruitListImg = [appleimg, banana, purple, mango, ceri];
+    const fruitListImgPutus = [appleputus, bananaputus, purpleputus, mangoputus, ceriputus];
+    const [fruit, setFruit] = useState([]);
+    const [fruitImg, setFruitImg] = useState([]);
+    const [fruitImgPutus, setFruitImgPutus] = useState([]);
 
-    const RandomFruit = (pick) => {
-        switch (pick) {
-            case 0: {
-                return (
-                    <>
-                        <div className="gamemembaca-wrap1">
-                            <img src={appleimg} alt="" />
-                            <h1>A-p-e-l</h1>
-                        </div>
-                        <div className="gamemembaca-wrap2">
-                            <img src={appleimg} alt=""/>
-                            <img src={appleputus} alt="" className="text" />
-                        </div>
-                    </>
-                )                
-                break;
-            }
-            case 1: {
-                return (
-                    <>
-                        <div className="gamemembaca-wrap1">
-                            <img src={banana} alt="" />
-                            <h1>A-p-e-l</h1>
-                        </div>
-                        <div className="gamemembaca-wrap2">
-                            <img src={banana} alt=""/>
-                            <img src={bananaputus} alt="" className="text" />
-                        </div>
-                    </>
-                )
-                break;
-            }
-            case 2: {
-                return (
-                    <>
-                        <div className="gamemembaca-wrap1">
-                            <img src={purple} alt="" />
-                            <h1>A-n-g-g-u-r</h1>
-                        </div>
-                        <div className="gamemembaca-wrap2">
-                            <img src={purple} alt=""/>
-                            <img src={purpleputus} alt="" className="text" />
-                        </div>
-                    </>
-                )
-                break;
-            }
-            case 3: {
-                return (
-                    <>
-                        <div className="gamemembaca-wrap1">
-                            <img src={mango} alt="" />
-                            <h1>M-a-n-g-g-a</h1>
-                        </div>
-                        <div className="gamemembaca-wrap2">
-                            <img src={mango} alt=""/>
-                            <img src={mangoputus} alt="" className="text" />
-                        </div>
-                    </>
-                )
-                break
-            }
-            case 4: {
-                return (
-                    <>
-                        <div className="gamemembaca-wrap1">
-                            <img src={ceri} alt="" />
-                            <h1>C-e-r-i</h1>
-                        </div>
-                        <div className="gamemembaca-wrap2">
-                            <img src={ceri} alt=""/>
-                            <img src={ceriputus} alt="" className="text" />
-                        </div>
-                    </>
-                )
-                break;
-            }
+    const randomUniqueNumber = (range, count) => {
+        let nums = new Set();
+        while (nums.size < count) {
+            nums.add(Math.floor(Math.random() * (range - 1 + 1) + 1));
         }
+        return [...nums];
     }
+
+    useEffect(() => {
+        let a = Math.floor(Math.random() * 5);
+        setFruit(fruitName[a]);
+        setFruitImg(fruitListImg[a]);
+        setFruitImgPutus(fruitListImgPutus[a]);
+    });
+
 
     return (
         <div className="gamemembaca">
             <div className="gamemembaca-container">
                 <div className="gamemembaca-wrapper">
-                    {RandomFruit(randomPick)}
+                    <div className="gamemembaca-wrap1">
+                            <img src={fruitImg} alt="" />
+                            <h1>{fruit}</h1>
+                    </div>
+                    <div className="gamemembaca-wrap2">
+                        <img src={fruitImg} alt=""/>
+                        <img src={fruitImgPutus} alt="" className="text" />
+                    </div>
                 </div>
-                <button>Next</button>
+                <button onClick={() => {
+                    let a = randomUniqueNumber(5, 5); 
+                    setFruit(fruitName[a]);
+                    setFruitImg(fruitListImg[a]);
+                    setFruitImgPutus(fruitListImgPutus[a]);}}
+                    >Next</button>
             </div>
         </div>
     )
