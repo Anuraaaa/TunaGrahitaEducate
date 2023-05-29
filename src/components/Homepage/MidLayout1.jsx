ea37bdd4ae20e0da45b88273021c0a7d8bd530d7
Anuraaaa <anwarfauzan75@gmail.com>
2023-05-30 00:46:45 +0700

    fix typo

--- src/components/Homepage/MidLayout1.jsx
+++ src/components/Homepage/MidLayout1.jsx
@@ -3,7 +3,7 @@ export default function MidLayout1 ({about}) {
         <div className="midlayout1" ref={about}>
             <h1>Tuna Grahita Educate</h1>
             <p>
-                Selamat datang di website pembelajaran khusus untuk anak-anak tunadaksa, tempat dimana pembelajaran
+                Selamat datang di website pembelajaran khusus untuk anak-anak tunagrahita, tempat dimana pembelajaran
                 menjadi menyenangkan, inklusif, dan sesuai dengan kebutuhan khusus mereka. Website ini didedikasikan untuk membantu
                 anak-anak dengan keterbatasan fisik dalam memperoleh pendidikan yang berkualitas, meningkatkan keterampilan mereka, dan
                 mengembangkan potensi maksimal
